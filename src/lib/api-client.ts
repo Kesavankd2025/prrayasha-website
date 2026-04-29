1223924f26b87d24cf7f28f529c4e17c9ed581f9
Kesavan0611 <kesavan@oceansoftteam.in>
2026-04-29 18:36:14 +0530

    url

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -4,8 +4,8 @@ import axios from "axios";
 // export const BASE_URL = "http://65.0.84.181:4000/api/website";
 // export const IMAGE_BASE_URL = "http://65.0.84.181:4000/public";
 
-export const BASE_URL = "http://localhost:5001/api/website";
-export const IMAGE_BASE_URL = "http://localhost:5001/public";
+export const BASE_URL = "https://prrayasha-backend.onrender.com/api/website";
+export const IMAGE_BASE_URL = "https://prrayasha-backend.onrender.com/public";
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
